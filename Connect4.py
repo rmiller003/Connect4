c8b7aedf03c0b2ceaaf83ddfaa9d97b1286856b3
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2020-07-13 14:43:51 -0400

    2nd Push

--- Connect4.py
+++ Connect4.py
@@ -5,16 +5,16 @@ COLUMN_COUNT = 7
 
 
 def create_board():
-    board = np.zeros((6, 7))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
 
 
 def drop_piece(board, row, col, piece):
-    board[row][col] ==piece
+    board[row][col] =piece
 
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 
 def get_next_open_row(board, col):
@@ -22,6 +22,26 @@ def get_next_open_row(board, col):
         if board[r][col] == 0:
             return r
 
+def print_board(board):
+    print(np.flip(board, 0))
+
+def winning_move(board, piece):
+    # Check horizontal location for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # Check for positive sloped diagonals
+
+
+    # Check for negative sloped diagonals
 
 board = create_board()
 game_over = False
@@ -36,6 +56,10 @@ while not game_over:
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
 
+            if winning_move(board, 1):
+                print("PLAYER 1 is the Winner!!!!! :)")
+                game_over = True
+
 
     # Ask for Player 2 Input
     else:
@@ -45,7 +69,7 @@ while not game_over:
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
 
-    print(board)
+    print_board(board)
 
     turn += 1
     turn = turn % 2
